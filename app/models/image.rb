dd1dbfb5cdb776ab84e930a1deea202b9046e72c
neurosis71 <neurosis71@hotmail.fr>
2015-04-09 15:10:20 +0200

    start of the admin integration : added the logo for the admin header and header styling

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,14 @@
 class Image < ActiveRecord::Base
   belongs_to :albums
 
+  #main image upload definition for paperclip
+  has_attached_file :main_image,
+                    :styles => {:medium => "300x300", :thumb => "100x100"},
+                    :path => ":rails_root/public/images/photos/:id/:style_:basename.:extension",
+                    :url => ":class/:id/:style_:basename.:extension",
+                    :default_url => "/images/errors/no_pic_available.jpg"
+  validates_attachment_content_type :main_image, :content_type => /\Aimage\/.*\Z/
+
   private
 
   def image_params
