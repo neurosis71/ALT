8bbc20b1bb7ba657d908459bc8a0fe0ef59d19d9
neurosis71 <neurosis71@hotmail.fr>
2015-05-12 09:14:14 +0200

    application about page basic integration

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,5 +2,15 @@ class AboutController < ApplicationController
   def index
 
     @page_title = t('app.about.title')
+    @about = About.find(1)
+    if I18n.locale == :fr
+      @about_text = @about.about_fr
+      @career_text = @about.career_fr
+      @interests_text = @about.interests_fr
+    else
+      @about_text = @about.about_en
+      @career_text = @about.career_en
+      @interests_text = @about.interests_en
+    end
   end
 end
