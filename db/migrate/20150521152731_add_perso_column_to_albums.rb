db3c7f256f0f318bf49d9329ec412a2ede869081
neurosis71 <neurosis71@hotmail.fr>
2015-06-02 18:26:52 +0200

    corrected default value type in migration to add personal flag to albums

--- db/migrate/20150521152731_add_perso_column_to_albums.rb
+++ db/migrate/20150521152731_add_perso_column_to_albums.rb
@@ -1,5 +1,5 @@
 class AddPersoColumnToAlbums < ActiveRecord::Migration
   def change
-    add_column :albums, :perso, :bool, :default => 0
+    add_column :albums, :perso, :bool, :default => false
   end
 end
